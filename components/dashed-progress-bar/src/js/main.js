f5edf358ce6769ed7abf9d41647d6e5f6d697168
Konrad Dzwinel <kdzwinel@gmail.com>
2016-07-01 11:26:53 +0200

    Retina progress bar (#31)

--- components/dashed-progress-bar/src/js/main.js
+++ components/dashed-progress-bar/src/js/main.js
@@ -4,8 +4,11 @@ import easeSin from './ease-sin';
 class DashedProgressBar {
   constructor({canvas, radius = 75, tiles = 30, gapSize = 0.25, animatedTiles = 10, startWidth = 2, endWidth = 12,
     animationStep = 0.015, backgroundColor = '#fff', inactiveTileColor = '#dde1e4'}) {
+    //support for HiDPIs
+    const dpi = window.devicePixelRatio || 1;
+
     // basic params
-    this.radius = radius;
+    this.radius = radius * dpi;
     this.tiles = tiles;
     this.gapSize = gapSize;//0-1
     this.backgroundColor = backgroundColor;
@@ -13,14 +16,17 @@ class DashedProgressBar {
 
     // animation params
     this.animatedTiles = animatedTiles;
-    this.startWidth = startWidth;
-    this.endWidth = endWidth;
+    this.startWidth = startWidth * dpi;
+    this.endWidth = endWidth * dpi;
     this.animationStep = animationStep;
 
     //canvas setup
-    this.margin = Math.max(this.startWidth, this.endWidth);
+    this.margin = Math.max(this.startWidth / 2, this.endWidth / 2);
     this.canvas = canvas;
-    this.canvas.width = this.canvas.height = this.radius * 2 + this.margin * 2;
+    const canvasSize = (this.radius * 2 + this.margin * 2);
+
+    this.canvas.width = this.canvas.height = canvasSize;
+    this.canvas.style.width = this.canvas.style.height = canvasSize / dpi + 'px';
     this.ctx = this.canvas.getContext('2d');
 
     //tiles
